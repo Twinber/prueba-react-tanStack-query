f09e1339eb9a32da63865bb0606e659c0db1f6c6
twinber <twinber@hotmail.com>
2026-08-04 07:45:54 +0200

    add rel=noopener noreferrer to external gmaps link

--- src/components/ui/GmapsButton.tsx
+++ src/components/ui/GmapsButton.tsx
@@ -11,7 +11,7 @@ export const GmapsButton: React.FC<Props> = ({latitude, longitude}) => {
     const {t} = useT()
 
     return (
-        <a target='_blank' href={`https://www.google.com/maps/place/${latitude},${longitude}`}>
+        <a target='_blank' rel='noopener noreferrer' href={`https://www.google.com/maps/place/${latitude},${longitude}`}>
             <Button>{t('show_in_gmaps')}</Button>
         </a>
     )
